bb57f60101c42682749ce9695dab4c3fba764d98
vitocorleone <dtdung0711@gmail.com>
2024-02-04 22:32:17 +0700

    thay đổi

--- components/techStack.ts
+++ components/techStack.ts
@@ -1,48 +1,48 @@
-	export type TechStack =
-	  | 'Python'
-	  | 'TypeScript'
-	  | 'JavaScript'
-	  | 'Java'
-	  | 'React'
-	  | 'NextJS'
-	  | 'Svelte'
-	  | 'TensorFlow'
-	  | 'PyTorch'
-	  | 'Streamlit'
-	  | 'PostgreSQL'
-	  | 'MySQL'
-	  | 'MongoDB'
-	  | 'Firebase'
-	  | 'FastAPI'
-	  | 'Docker'
-	  | 'Git'
-	  | 'Prisma'
-	  | 'Drizzle'
-	  | 'PHP'
-	  | 'Html'
-	  | 'Css'
+export type TechStack =
+  | 'Python'
+  | 'TypeScript'
+  | 'JavaScript'
+  | 'Java'
+  | 'React'
+  | 'NextJS'
+  | 'Svelte'
+  | 'TensorFlow'
+  | 'PyTorch'
+  | 'Streamlit'
+  | 'PostgreSQL'
+  | 'MySQL'
+  | 'MongoDB'
+  | 'Firebase'
+  | 'FastAPI'
+  | 'Docker'
+  | 'Git'
+  | 'Prisma'
+  | 'Drizzle'
+  | 'PHP'
+  | 'Html'
+  | 'Css'
 
-	export const techStack: TechStack[] = [
-	  'Python',
-	  'TypeScript',
-	  'JavaScript',
-	  'Java',
-	  'React',
-	  'NextJS',
-	  'Svelte',
-	  'TensorFlow',
-	  'PyTorch',
-	  'Streamlit',
-	  'PostgreSQL',
-	  'MySQL',
-	  'MongoDB',
-	  'Firebase',
-	  'FastAPI',
-	  'Docker',
-	  'Git',
-	  'Prisma',
-	  'Drizzle',
-	  'PHP',
-	  'Html',
-	  'Css'
-	]
+export const techStack: TechStack[] = [
+  'Python',
+  'TypeScript',
+  'JavaScript',
+  'Java',
+  'React',
+  'NextJS',
+  'Svelte',
+  'TensorFlow',
+  'PyTorch',
+  'Streamlit',
+  'PostgreSQL',
+  'MySQL',
+  'MongoDB',
+  'Firebase',
+  'FastAPI',
+  'Docker',
+  'Git',
+  'Prisma',
+  'Drizzle',
+  'PHP',
+  'Html',
+  'Css',
+]
